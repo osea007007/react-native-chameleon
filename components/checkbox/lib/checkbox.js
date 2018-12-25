9b8a139b87e0d89c09ba8430b2881b56acdf070a
jason.xing@thizgroup.com <jason.xing@thizgroup.com>
2018-12-25 17:58:03 +0800

    test

--- components/checkbox/lib/checkbox.js
+++ components/checkbox/lib/checkbox.js
@@ -6,6 +6,7 @@ import propTypes from 'prop-types';
  * page: test
  * ---
  *
+ *
  * 请使用
  */
 export class Checkbox extends Component {
@@ -16,20 +17,6 @@ export class Checkbox extends Component {
 
 
 Checkbox.propTypes = {
-    /**
-     * test must be true
-     */
-    test:propTypes.number.isRequired
-};
-
-export class Checkbox1 extends Component {
-    render() {
-        return null;
-    }
-}
-
-
-Checkbox1.propTypes = {
     /**
      * test must be false
      */
