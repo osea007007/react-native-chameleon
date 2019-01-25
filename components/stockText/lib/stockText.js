bf59303c3f66c121f1efe814d81825856784f611
jinsongpei <songpei.jin@thizgroup.com>
2019-01-25 17:53:06 +0800

    stocktext 优化

--- components/stockText/lib/stockText.js
+++ components/stockText/lib/stockText.js
@@ -12,12 +12,30 @@ import Proptypes from 'prop-types';
  */
 
 class StockText extends Component {
+
+    isLargerZero = (value) => {
+        let temp = Number(value);
+
+        return temp >= 0;
+    };
+
+    display = (value) => {
+        if(isNaN(value)) {      //非法字符
+            return '--';
+        } else {
+            let temp = Number(value);
+            if(temp >= 0) {
+                return '+' + temp;
+            } else {
+                return temp;
+            }
+        }
+    };
+
     render(){
-        let {containerStyle, value, fontSize} = this.props;
+        let {value, fontSize, tailCharacter=''} = this.props;
         return(
-            <View style={[StockTextStyle.container, containerStyle]}>
-                <Text style={[StockTextStyle.text, {color: value ? (value.indexOf('-') === -1 ? 'red' : 'green') : 'black', fontSize:fontSize}]}>{ value ? value+'' : '--' }</Text>
-            </View>
+            <Text style={[StockTextStyle.text, {color: value ? (this.isLargerZero(value) ? 'red' : 'green') : 'black', fontSize:fontSize}]}>{ value ? this.display(value)+tailCharacter : ' ' }</Text>
         );
     }
 }
@@ -34,7 +52,7 @@ StockText.propTypes = {
     /**
      * 当前数值,传入字符串类型
      */
-    value:Proptypes.string,
+    value:Proptypes.any,
     /**
      * 字体大小
      */
